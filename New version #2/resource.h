d786e5cc7fa8c1ba707a1de7ea2ff2f247a351f1
nix887 <nix887nix@gmail.com>
2026-05-19 02:25:13 +0300

    Add files via upload

---
+++ New version #2/resource.h
@@ -0,0 +1,36 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется PinAnalyser.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_PINANALYSER_DIALOG          102
+#define IDR_MAINFRAME                   128
+#define IDC_STATIC_TYPE                 1000
+#define IDC_RADIO_4                     1001
+#define IDC_RADIO_6                     1002
+#define IDC_STATIC_PIN                  1003
+#define IDC_EDIT_PIN                    1005
+#define IDC_BTN_LOAD                    1006
+#define IDC_BTN_ANALYZE                 1007
+#define IDC_LIST_GOOD                   1008
+#define IDC_LIST_MEDIUM                 1009
+#define IDC_STATIC_MEDIUM               1010
+#define IDC_STATIC_GOOD                 1011
+#define IDC_STATIC_BAD                  1012
+#define IDC_LIST_GOOD2                  1013
+#define IDC_LIST_BAD                    1013
+#define IDC_BUTTON1                     1015
+#define IDC_BTN_SAVE                    1014
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
